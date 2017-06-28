79cfd292ca62f62a01eb9b661fdca990ab1a9011
jillrizley <jillian.rizley@gmail.com>
2017-06-28 09:31:54 -0400

    can add x to boxes, some css, sign in/out

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,24 @@
 'use strict'
-
+//
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 
-$(() => {
-  setAPIOrigin(location, config)
+const events = require('./events')
+// const events = require('./events')
+
+// // $(() => {
+// //   setAPIOrigin(location, config)
+// //   $('#sign-up-form').on('submit', events.onSubmit)
+// // })
+// //
+// // require('./example')
+
+// make an x appear in a box when clicked
+$('#game-board').on('click', function (event) {
+  $(event.target).text('X', event.target)
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-require('./example')
+// change every other x to an o
+// end the game when there are no more spaces left
+// declare a winner
+// reset the game
